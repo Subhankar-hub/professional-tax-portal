c5601bb7c3b29c106c73e5063b72a97fa13ee0ef
himanshusachan112 <hs5050407@gmail.com>
2025-06-21 18:11:42 +0530

    Initial commit for backend

---
+++ taxprotal_backend/src/main/java/com/taxprotal_backend/taxprotal_backend/TaxprotalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.taxprotal_backend.taxprotal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxprotalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxprotalBackendApplication.class, args);
+	}
+
+}
---
+++ taxprotal_backend/src/test/java/com/taxprotal_backend/taxprotal_backend/TaxprotalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taxprotal_backend.taxprotal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxprotalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
