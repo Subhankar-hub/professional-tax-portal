6e8c9c5f7a429c2f903cf1d8c75b0c28cfcba528
Subhankar Roy <subhankar.smith2003@gmail.com>
2025-06-22 16:21:29 +0530

    feat(ptax): add Charge JPA entity for mtbl_charge table

---
+++ professionaltaxportal/src/main/java/io/example/professionaltaxportal/entity/Charge.java
@@ -0,0 +1,27 @@
+package io.example.professionaltaxportal.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "mtbl_charge", schema = "ptax")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Charge {
+
+    @Id
+    @Column(name = "code", length = 4)
+    private String code;
+
+    @Column(name = "charge", length = 255)
+    private String charge;
+
+    @Column(name = "area_code", length = 3)
+    private String areaCode;
+
+    @Column(name = "charge_sn")
+    private Integer chargeSn;
+}
